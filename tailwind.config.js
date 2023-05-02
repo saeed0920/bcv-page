a74c83f04b2447e55e18c12bf960a8ef5ca17c50
Saeed0920 <sd.eed1381@gmail.com>
2023-05-02 17:08:55 +0330

    finish section of footer and faq

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        faq: "1fr 5fr 2rem",
+      },
       colors: {
         bcvGreen: "#65e6cf",
         bcvGray: "#cdcfd3",
